42b8892e1bd13d280a8923043c75b2bec9642b49
jameslol417 <jameslol4177@gmail.com>
2021-12-06 15:13:52 +0900

    modified users,rooms,reviews,reservations

--- config/settings.py
+++ config/settings.py
@@ -39,9 +39,15 @@ DJANGO_APPS = [
     "django.contrib.staticfiles",
 ]
 
-THIRD_PARTY_APPS = []
+THIRD_PARTY_APPS = ["django_countries"]
 
-PROJECT_APPS = ["users.apps.UsersConfig"]
+PROJECT_APPS = [
+    "core.apps.CoreConfig",
+    "users.apps.UsersConfig",
+    "rooms.apps.RoomsConfig",
+    "reviews.apps.ReviewsConfig",
+    "reservations.apps.ReservationsConfig",
+]
 
 INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS
 
--- rooms/models.py
+++ rooms/models.py
@@ -1,3 +1,89 @@
+from django.contrib.admin.decorators import register
 from django.db import models
+from django.db.models.fields import BLANK_CHOICE_DASH
+from django_countries.fields import CountryField
+from core import models as core_models
 
-# Create your models here.
+
+class AbstractItem(core_models.TimeStampedModel):
+
+    """Abstract Item"""
+
+    name = models.CharField(max_length=80)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.name
+
+
+class RoomType(AbstractItem):
+
+    """RoomType Model Definition"""
+
+    class Meta:
+        verbose_name = "Room Type"
+
+
+class Amenity(AbstractItem):
+
+    """Amenity Model Definition"""
+
+    class Meta:
+        verbose_name_plural = "Amenities"
+
+
+class Facility(AbstractItem):
+
+    """Facility Model Definition"""
+
+    class Meta:
+        verbose_name_plural = "Facilities"
+
+
+class HouseRule(AbstractItem):
+
+    """Facility Model Definition"""
+
+    class Meta:
+        verbose_name = "House Rule"
+
+
+class Photo(core_models.TimeStampedModel):
+
+    """Photo Model Definition"""
+
+    caption = models.CharField(max_length=80)
+    file = models.ImageField()
+    room = models.ForeignKey("Room", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.caption
+
+
+class Room(core_models.TimeStampedModel):
+
+    """Room Model Definition"""
+
+    name = models.CharField(null=True, max_length=140)
+    description = models.TextField(null=True)
+    country = CountryField(null=True)
+    city = models.CharField(null=True, max_length=80)
+    price = models.IntegerField(null=True)
+    address = models.CharField(null=True, max_length=140)
+    guests = models.IntegerField(null=True)
+    beds = models.IntegerField(null=True)
+    bedrooms = models.IntegerField(null=True)
+    baths = models.IntegerField(null=True)
+    check_in = models.TimeField(null=True)
+    check_out = models.TimeField(null=True)
+    instant_book = models.BooleanField(default=False)
+    host = models.ForeignKey("users.User", on_delete=models.CASCADE, null=True)
+    room_type = models.ForeignKey("RoomType", on_delete=models.SET_NULL, null=True)
+    amenities = models.ManyToManyField("Amenity", blank=True)
+    facilities = models.ManyToManyField("Facility", blank=True)
+    house_rules = models.ManyToManyField("HouseRule", blank=True)
+
+    def __str__(self):
+        return self.name
