3cfa1b2b18fcdd9833a9f340c80520d6ed2c4b16
jameslol417 <jameslol4177@gmail.com>
2021-12-17 16:20:55 +0900

    Finished Github Login

--- config/settings.py
+++ config/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-#28@@cil+5ld98xdt#l@uy6y(unm#xo=w%a4twutw_zzlzl79m"
+SECRET_KEY = os.environ.get("DJANGO_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
